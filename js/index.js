6e152f1ccfc08bca959abebbc515264ff6cfbd58
Vitor Domingos <vm486092@gmail.com>
2023-10-19 17:52:38 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+// Declaração de uma variável chamada 'boletoPago' com o valor 'false', indicando que o boleto não está pago.
+const boletoPago = false;
+
+// Verifica se 'boletoPago' é igual a 'true' (significa que o boleto está pago) ou 'false' (significa que o boleto não está pago).
+if (boletoPago) {
+    // Se 'boletoPago' for igual a 'true', exibe a mensagem informando que o boleto está pago.
+    console.log("O boleto está pago.");
+} else {
+    // Se 'boletoPago' for igual a 'false', exibe a mensagem informando que o boleto não está pago.
+    console.log("O boleto não está pago.");
+}
